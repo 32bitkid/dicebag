1f360ae6e50395b3c316a61cce57a8f0ca81133e
Jim Holmes <32bitkid@gmail.com>
2013-03-17 19:57:37 -0400

    refactored 10 sided die tests.

--- test/d10_tests.js
+++ test/d10_tests.js
@@ -1,17 +1,18 @@
 var assert = require('assert');
 var DiceBag = require('../dicebag');
 var sequence = require('./sequence');
+var faces = require('./faces').tenSided
 
 var bag;
 
 describe("rolling a d10", function() {
 
 	it("should be a minimum of 1", function() {
-		bag = new DiceBag(sequence(0));
+		bag = new DiceBag(sequence(faces.face1));
 		assert.equal(bag.d10(), 1);
 	})
 	it("should be a maximum of 10", function() {
-		bag = new DiceBag(sequence(0.999999));
+		bag = new DiceBag(sequence(faces.face10));
 		assert.equal(bag.d10(), 10);
 	})
 });
--- test/faces.js
+++ test/faces.js
@@ -1,11 +1,18 @@
 faces = {}
 
 var generateFaces = function(name, sides) {
+	var value;
+
 	faces[name] = {};
-	for(var i = 0; i < sides; i++)
-		faces[name]["face"+(i+1)] = i/sides;
+	faces[name]["sequence"] = []
+	for(var i = 0; i < sides; i++) {
+		value = i/sides;
+		faces[name]["face"+(i+1)] = value;
+		faces[name]["sequence"].push(value);
+	}
 }
 
-generateFaces("sixSided", 6)
+generateFaces("sixSided", 6);
+generateFaces("tenSided", 10);
 
 module.exports = faces
