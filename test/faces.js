f865a51c48b66ae74b823fda3d4f402395764f99
Jim Holmes <32bitkid@gmail.com>
2013-03-17 19:10:22 -0400

    refactoring face harness

--- test/faces.js
+++ test/faces.js
@@ -1,12 +1,11 @@
 faces = {}
 
-module.exports = {
-	sixSided: {
-		face1: 0,
-		face2: 1/6,
-		face3: 2/6,
-		face4: 3/6,
-		face5: 4/6,
-		face6: 5/6
-	}
+var generateFaces = function(name, sides) {
+	faces[name] = {};
+	for(var i = 0; i < sides; i++)
+		faces[name]["face"+(i+1)] = i/sides;
 }
+
+generateFaces("sixSided", 6)
+
+module.exports = faces
