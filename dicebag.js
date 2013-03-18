7fd5d907ead2ba1bc8c73e7dbbf45f00a540830c
Jim Holmes <32bitkid@gmail.com>
2013-03-17 21:20:41 -0400

    refactor, and create instance bound methods instead of prototypical methods.

--- dicebag.js
+++ dicebag.js
@@ -1,11 +1,48 @@
 var DiceBag = (function() {
+
+	var standardDieLookup = function(value) { return value + 1; }
+	var createDie = function(context, random , sides) {
+		var actualSides;
+		var lookupFn;
+
+		if(typeof(sides) === "number") {
+			actualSides = sides;
+			lookupFn = standardDieLookup;
+		}
+		else if(sides.length) {
+			actualSides = sides.length;
+			lookupFn = function(value) {
+				return sides[value];
+			};
+		}
+
+		var roll = function(times) {
+			times = times || 1;
+
+			var result = new RollResult();
+			var dieResult;
+
+			for(var i = 0; i < times; i++) {
+				dieResult = Math.round(random() * actualSides);
+				result.addRoll(lookupFn(dieResult));
+			}
+
+			return result;
+		};
+
+		return function() { return roll.apply(context, arguments); }
+	}
+
 	function DiceBag(random) {
 		this.random = random || Math.random;
+
+		this.d6 = createDie(this, random, 6);
+		this.d10 = createDie(this, random, 10);
 		this.efron = {
-			red: createDie({sides:[4,4,4,4,0,0], random: random}),
-			green: createDie({sides:[3,3,3,3,3,3], random: random}),
-			blue: createDie({sides:[6,6,2,2,2,2], random: random}),
-			purple: createDie({sides:[5,5,5,1,1,1], random: random})
+			red: createDie(this, random, [4,4,4,4,0,0]),
+			green: createDie(this, random, [3,3,3,3,3,3]),
+			blue: createDie(this, random, [6,6,2,2,2,2]),
+			purple: createDie(this, random, [5,5,5,1,1,1])
 		};
 	}
 
@@ -20,41 +57,6 @@ var DiceBag = (function() {
 	};
 
 	RollResult.prototype.valueOf = function() { return this.total; };
-
-	var standardDieLookup = function(value) { return value + 1; }
-	var createDie = function(options) {
-		var actualSides;
-		var lookupFn;
-
-		if(typeof(options.sides) === "number") {
-			actualSides = options.sides;
-			lookupFn = standardDieLookup;
-		}
-		else if(options.sides.length) {
-			actualSides = options.sides.length;
-			lookupFn = function(value) {
-				return options.sides[value];
-			};
-		}
-
-		return function(times) {
-			times = times || 1;
-
-			var random = options.random || this.random;
-			var result = new RollResult();
-			var dieResult;
-
-			for(var i = 0; i < times; i++) {
-				dieResult = Math.round(random() * actualSides);
-				result.addRoll(lookupFn(dieResult));
-			}
-
-			return result;
-		};
-	}
-
-	DiceBag.prototype.d6 = createDie({sides:6});
-	DiceBag.prototype.d10 = createDie({sides:10});
 
 	return DiceBag;
 })();
